657ee619093648b463002b05b9fef576b0f1b0c3
Gabriella Manfredo <gabriella.manfredo@gmail.com>
2024-04-29 18:42:30 -0300

    feat: adicionando navbar, footer, home e rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import Home from './paginas/home/Home';
-import './App.css';
-
+import React from 'react'
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-   <Home />
-  );
-}
+    <>
 
-export default App;
+    {/* Satélite - ele habilita o sistema de localização/roteamento */}
+      <BrowserRouter>
+        <Navbar/>
+
+        <div>
+
+        </div>
+          <Routes>
+
+            {/* Rota para chegar no objetivo */}
+            <Route path = '/' element = { <Home/>} />
+            <Route path = '/' element = { <Login/>} />
+            <Route path = '/' element = { <Home/>} />
+              
+          </Routes>
+        {/* GPS/Aplicativo -> ele pega o caminho da URL (login/cadastro) */}
+
+        <Footer/>
+
+      </BrowserRouter>
+    </>
+)
+}
+export default App
